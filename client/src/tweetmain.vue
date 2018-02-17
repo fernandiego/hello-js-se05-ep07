f8a65a365d9606e66269a401c8b0d5944e546106
Fox <fernandodiego.sn@gmail.com>
2018-02-17 05:53:00 -0300

    Identando o código

--- client/src/tweetmain.vue
+++ client/src/tweetmain.vue
@@ -1,13 +1,13 @@
 <template>
-<nav class="panel">
-  <p class="panel-heading">
-    Crudão
-  </p>
-  <div class="panel-block">
-
-<form @submit.prevent="dosave">
-    <div class="field">
-  <label class="label">Login:</label>
+  <nav class="panel">
+    <p class="panel-heading">
+      Crudão
+    </p>
+  
+<div class="panel-block">
+  <form @submit.prevent="dosave">
+  <div class="field">
+    <label class="label">Login:</label>
   <div class="control">
     <input class="input" type="text" placeholder="Text input" v-model="usuario.usuariologin">
   </div>
@@ -19,47 +19,49 @@
     <input class="input" type="text" placeholder="Text input" v-model="usuario.usuarionome">
   </div>
 </div> 
-          <div class="field">
+
+<div class="field">
   <label class="label">Telefone:</label>
   <div class="control">
     <input class="input" type="text" placeholder="Text input" v-model="usuario.usuariotelefone">
   </div>
 </div> 
-          <div class="field">
-  <label class="label">Data de Nascimento:</label>
-  <div class="control">
-    <input required class="input" type="text" placeholder="Text input" v-model="usuario.usuariodtnasc">
-  </div>
-</div> 
-<div class="field is-grouped">
-  <div class="control">
-    <button class="button is-link" type="submit">Submit</button>
-  </div>
-</div>  
+
+    <div class="field">
+      <label class="label">Data de Nascimento:</label>
+      <div class="control">
+        <input required class="input" type="text" placeholder="Text input" v-model="usuario.usuariodtnasc">
+      </div>
+    </div> 
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link" type="submit">Submit</button>
+      </div>
+    </div>  
   </form>
+</div>
 
-  </div>
-
- <div class="panel-block"> 
-  <ul>
-      <li v-for="u in usuarios" :key="u.usuarioid">
+  <div class="panel-block"> 
+      <ul>
+        <li v-for="u in usuarios" :key="u.usuarioid">
           <div class="columns">
           <div class="column"> {{u.usuarionome}} </div>
           <div class="column">
               <button @click="usuario=u" class="button is-info">Editar</button>
           </div>
-          <div class="column">
+            <div class="column">
               <button @click="delet(u)" class="button is-danger">Excluir</button>
+            </div>
           </div>
-          </div>
-      </li>
-   </ul>
- </div>
-</nav>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-const { usuarioapi } = require("./api")
+const { usuarioapi } = require("./api");
 module.exports = {
   name: "usuario",
   created() {
